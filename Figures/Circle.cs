26b73e35d4d1110b1221c8afbe64319655f99492
Lisyaka46 <killmeisfox@mail.ru>
2024-10-12 19:42:06 +0300

    Добавьте файлы проекта.

---
+++ Figures/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularSquare.Figures
+{
+    /// <summary>
+    /// Управление фигурой круга
+    /// </summary>
+    public class Circle
+    {
+        /// <summary>
+        /// Вычисление площади круга
+        /// <code>
+        /// PI * Radius ^ 2
+        /// </code>
+        /// </summary>
+        /// <param name="Radius">Радиус данного круга</param>
+        /// <exception cref="ArgumentException">Исключение при отрицательном значении радиуса</exception>
+        /// <returns>Вычисленная площадь</returns>
+        public static double SquareCalculate(double Radius)
+        {
+            if (Radius < 0d) throw new ArgumentException($"Радиус круга не может быть отрицательным! ({nameof(Radius)}={Radius})");
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
---
+++ Figures/Triangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularSquare.Figures
+{
+    /// <summary>
+    /// Управление фигурой треугольника
+    /// </summary>
+    public class Triangle
+    {
+        /// <summary>
+        /// Вычислить площадь треугольника по трём сторонам
+        /// <code>
+        /// √(р * (р — Foot) * (р — СatheterLeft) * (p — СatheterRight))
+        /// </code>
+        /// </summary>
+        /// <param name="Hypotenuse">Гипотенуза треугольника</param>
+        /// <param name="СatheterLeft">Левый катет треугольника</param>
+        /// <param name="СatheterRight">Правый катет треугольника</param>
+        /// <returns>Вычисленная площадь треугольника</returns>
+        /// <exception cref="ArgumentException">Исключение при отричательной длинне одной из сторон треугольника</exception>
+        /// <exception cref="Exception">Исключение при несуществующем треугольнике</exception>
+        public static double SquareCalculate(double Hypotenuse, double СatheterLeft, double СatheterRight)
+        {
+            if (Hypotenuse < 0d || СatheterLeft < 0d || СatheterRight < 0d) throw new ArgumentException("Значения сторон треугольника не могут быть отрицательными! " +
+                $"({nameof(Hypotenuse)}={Hypotenuse}, {nameof(СatheterLeft)}={СatheterLeft}, {nameof(СatheterRight)}={СatheterRight})");
+
+            double HalfMeter = (Hypotenuse + СatheterLeft + СatheterRight) / 2d; // полупериметр
+
+            if (HalfMeter < Hypotenuse || HalfMeter < СatheterLeft || HalfMeter < СatheterRight)
+                throw new Exception("Такого треугольника не существует! " +
+                $"({nameof(Hypotenuse)}={Hypotenuse}, {nameof(СatheterLeft)}={СatheterLeft}, {nameof(СatheterRight)}={СatheterRight})");
+            return Math.Sqrt(HalfMeter * (HalfMeter - Hypotenuse) * (HalfMeter - СatheterLeft) * (HalfMeter - СatheterRight));
+        }
+
+        /// <summary>
+        /// Узнать соответствует ли данный треугольник прямоугольному типу
+        /// <code>
+        /// Foot ^ 2 == СatheterLeft ^ 2 + СatheterRight ^ 2
+        /// </code>
+        /// </summary>
+        /// <param name="Hypotenuse">Гипотенуза треугольника</param>
+        /// <param name="СatheterLeft">Левый катет треугольника</param>
+        /// <param name="СatheterRight">Правый катет треугольника</param>
+        /// <returns>Соответствие треугольника с прямоугольным</returns>
+        /// <exception cref="ArgumentException">Исключение при отричательной длинне одной из сторон треугольника</exception>
+        public static bool CheckRectangularTriangle(double Hypotenuse, double СatheterLeft, double СatheterRight)
+        {
+            if (Hypotenuse < 0d || СatheterLeft < 0d || СatheterRight < 0d) throw new ArgumentException("Значения сторон треугольника не могут быть отрицательными! " +
+                $"({nameof(Hypotenuse)}={Hypotenuse}, {nameof(СatheterLeft)}={СatheterLeft}, {nameof(СatheterRight)}={СatheterRight})");
+            return Math.Pow(Hypotenuse, 2) == Math.Pow(СatheterLeft, 2) + Math.Pow(СatheterRight, 2);
+        }
+    }
+}
